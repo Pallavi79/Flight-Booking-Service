a1719f6d42e46ed2079d5ddd4bb2ad15e70bfae8
Pallavi <pallavi3749@gmail.com>
2023-10-19 15:51:38 +0530

    initial configuration

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {ServerConfig, Logger} = require('./config');
+const apiRoutes = require('./routes');
+
+const app = express();
+
+//let express know how to read re body . parse the incoming json req body
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api',apiRoutes);
+
+
+app.listen(ServerConfig.PORT, ()=>{
+    console.log(`Successfully started the server on port ${ServerConfig.PORT}`);
+    //Logger.info("Successfully started the server",{});
+
+});
+
