d19c3e23b1ee45986de7ed57188591f59f387c33
Pallavi <pallavi3749@gmail.com>
2023-10-20 15:28:00 +0530

    update seats after booking

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const {InfoController} = require('../../controllers');
 router.get('/info',InfoController.info);
 
+const bookingRoutes = require('./booking')
+router.use('/bookings',bookingRoutes);
 
 module.exports = router;
